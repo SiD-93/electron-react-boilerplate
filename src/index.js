4333cd67409e6c7a9b58ea0758ba0a1ca19dac52
Siddharth Ramesh <siddharth.ramesh93@gmail.com>
2016-12-31 00:51:05 +0530

    Adding React Router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+
 import Main from './components/Main';
 
 ReactDOM.render(
-  <Main />,
+  <Router history={hashHistory}>
+    <Route path='/' component={Main} />
+  </Router>,
   document.getElementById('root')
 );
